5113399508e9ad1af097189de1acde5237692f1d
lQAQl0300 <rickychouw@gmail.com>
2023-12-02 05:01:23 +0800

    mod

--- OS/hw6/hw6_1.c
+++ OS/hw6/hw6_1.c
@@ -2,16 +2,24 @@
 #include<stdlib.h>
 #include<pthread.h>
 
+int SIZE = 0;
+int arr[10000];
+
 void* merge_sort(void* data){
-    
+    int *mid = (int*) data;
+    pthread_t left, right;
+    pthread_create()
 }
 
 int main(int argc, char **argv){
     FILE* fpin = fopen(argv[1], "r");
     FILE* fpout = fopen(argv[2], "w");
+    while(fscanf(fpin, "%d", &arr[SIZE]) == 1) SIZE++;
 
-    pthread_t left, right;
-    pthread_create(left, NULL, merge_sort, );
+    int mid = SIZE / 2;
+
+    pthread_t t;
+    pthread_create(t, NULL, merge_sort, mid);
 
     return 0;
 }
