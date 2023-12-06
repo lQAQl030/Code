7daca1a69c319feea84685ca7cb600da8e69a364
Ricky <rickychouw@gmail.com>
2023-12-07 00:11:01 +0800

    OS hw7

--- OS/hw7/hw7_write1.c
+++ OS/hw7/hw7_write1.c
@@ -7,36 +7,36 @@
 #include <sys/types.h>
 
 
-const char *string_to_write = "hw6-1, 311551xxx";   // Task0: Change this string with your own student ID.
+const char *string_to_write = "hw6-1, 110704010";   // Task0: Change this string with your own student ID.
 
 int main() {
     // 1. Create and Open your own shared file
-    int fd = open("shared_file1", , );               // Task1: Complete open() function.
+    int fd = open("shared_file1", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);               // Task1: Complete open() function.
     if (fd == -1) {
         perror("open");
         return 1;
     }
     
     // 2. Set the size of shared file
-    int data_size = ;                               // Task2: Complete data_size.
+    int data_size = strlen(string_to_write);                             // Task2: Complete data_size.
     if (ftruncate(fd, data_size) == -1) {
         perror("ftruncate");
         return 1;
     }
     
     // 3. Use mmap() to map the memory
-    char *shared_data = mmap(, , , , , );           // Task3: Complete mmap() function.
+    char *shared_data = mmap(NULL, data_size, PROT_WRITE, MAP_SHARED, fd, 0);           // Task3: Complete mmap() function.
     if (shared_data == MAP_FAILED) {
         perror("mmap failed\n");
         return 1;
     }
 
     // 4. Write data
-    strcpy( , );                                    // Task4: Complete write data part.
+    strcpy(shared_data, string_to_write);                                    // Task4: Complete write data part.
 
     
     // 5. Use munmap() to un map the memory
-    if (munmap(, ) == -1) {                         // Task5: Complete munmap() function.
+    if (munmap(shared_data, data_size) == -1) {                         // Task5: Complete munmap() function.
         perror("munmap failed\n");
         return 1;
     }
