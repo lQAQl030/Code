7261c8cb2dc58ef0bdf4d674a42bd057de715261
Ricky <rickychouw@gmail.com>
2023-12-15 12:31:01 +0800

    daily leetcode

---
+++ LeetCode/1436. Destination City.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> s;
+        for(vector<string> &v : paths){
+            s.insert(v[0]);
+        }
+
+        for(vector<string> &v : paths){
+            if(s.find(v[1]) == s.end()) return v[1];
+        }
+        return "";
+    }
+};
