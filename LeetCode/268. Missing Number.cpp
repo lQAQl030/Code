283d4024edb86917662244df25929316d2acd785
Ricky <rickychouw@gmail.com>
2024-02-20 16:51:29 +0800

    daily leetcode

---
+++ LeetCode/268. Missing Number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size() * (nums.size() + 1) / 2;
+        for(int &i : nums) ans -= i;
+        return ans;
+    }
+};
