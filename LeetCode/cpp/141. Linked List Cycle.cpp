f8f80817f368b8f5685f794b2bd7cc65b50c555a
Ricky <rickychouw@gmail.com>
2024-03-06 14:35:25 +0800

    daily leetcode

--- LeetCode/cpp/141. Linked List Cycle.cpp
+++ LeetCode/cpp/141. Linked List Cycle.cpp
@@ -1,26 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-	
-		// making two pointers fast and slow and assignning them to head
         ListNode *fast = head;
         ListNode *slow = head;
-        
-		// till fast and fast-> next not reaches NULL
-		// we will increment fast by 2 step and slow by 1 step
-        while(fast != NULL && fast ->next != NULL)
-        {
+        while(fast && fast->next){
             fast = fast->next->next;
             slow = slow->next;
-            
-			
-			// At the point if fast and slow are at same address
-			// this means linked list has a cycle in it.
-            if(fast == slow)
-                return true;
+            if(fast == slow) return true;
         }
-        
-		// if traversal reaches to NULL this means no cycle.
         return false;
     }
 };
