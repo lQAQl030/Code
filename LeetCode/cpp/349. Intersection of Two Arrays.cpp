67edc6e1bd3938d6d44c28261271dc97b261f2d5
Ricky <rickychouw@gmail.com>
2024-03-10 16:48:52 +0800

    daily leetcode

---
+++ LeetCode/cpp/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+        vector<int> ans;
+        while(i < nums1.size() && j < nums2.size()){
+            if(i>0 && nums1[i]==nums1[i-1]){
+                i++;
+                continue;
+            }
+
+            if(nums1[i] == nums2[j]){
+                ans.push_back(nums1[i]);
+                i++; j++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+        
+        return ans;
+    }
+};
