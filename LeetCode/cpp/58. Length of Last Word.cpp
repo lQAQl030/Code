60a180091099c9b46d8d7a69564ae317ba5b34f0
Ricky <rickychouw@gmail.com>
2024-04-02 16:18:10 +0800

    daily leetcode

--- LeetCode/cpp/58. Length of Last Word.cpp
+++ LeetCode/cpp/58. Length of Last Word.cpp
@@ -25,3 +25,16 @@ public:
         return count;
     }
 };
+
+//second try
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int n = s.size();
+        int i = n - 1;
+        while(i >= 0 && s[i] == ' ') i--;
+        while(i >= 0 && s[i--] != ' ') count++;
+        return count;
+    }
+};
